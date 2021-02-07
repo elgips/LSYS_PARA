46f2b6c160a4e53b129bc18897a2127f03488b9d
Elad <72931343+elgips@users.noreply.github.com>
2021-02-07 20:46:41 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,21 +6,22 @@
  */
 #include <stdio.h>
 #include "LSYS_PARA.h"
-
+#include<fstream>
+#include<sstream>
 
 int main(){
 	string s="A(()())B((()())())";
 
-//	string s="A(@{x,y}x*y,@{z}z^2):0.5,[B(@{x,y}x+y,@{z}z-3)+C(@{x,y}x-y,@{z}1/z)]:0.25,G(@{q}sin(q)):0.25";
-//	size_t x=CloseLoc(s, ',',0);
-//	successorsSpace pS(s);
-//	char c=')';
-//	size_t CL=s.rfind(c);
-//	cout<< CL <<endl;
-//	size_t OL=CloseLoc(s, c, CL);
-//	cout<< OL <<endl;
-//	cout<< s.substr(OL,CL) <<endl;
-		string L_st=file2string("../LSYS_EXPRTK_STO/models/model1_9.txt");
+	//	string s="A(@{x,y}x*y,@{z}z^2):0.5,[B(@{x,y}x+y,@{z}z-3)+C(@{x,y}x-y,@{z}1/z)]:0.25,G(@{q}sin(q)):0.25";
+	//	size_t x=CloseLoc(s, ',',0);
+	//	successorsSpace pS(s);
+	//	char c=')';
+	//	size_t CL=s.rfind(c);
+	//	cout<< CL <<endl;
+	//	size_t OL=CloseLoc(s, c, CL);
+	//	cout<< OL <<endl;
+	//	cout<< s.substr(OL,CL) <<endl;
+	string L_st=file2string("../LSYS_EXPRTK_STO/models/example1_4_1.txt");
 	LSYS L(L_st);
 	L.propagate();
 	cout<<L.current<< endl;
@@ -30,8 +31,15 @@ int main(){
 	cout<<L.current<< endl;
 	L.propagate();
 	cout<<L.current<< endl;
-//	unsigned int n=50;
-//	L.simulate(n);
-//	cout<<L.current<< endl;
+	L.propagate();
+	cout<<L.current<< endl;
+	L.propagate();
+	cout<<L.current<< endl;
+//		unsigned int n=12;
+//		L.simulate(n);
+//		cout<<L.current<< endl;
+	ofstream out("../streamTree3D/LSYOUT.txt", ios::out );
+	out<<L.current;
+	out.close();
 	return 0;
 }
